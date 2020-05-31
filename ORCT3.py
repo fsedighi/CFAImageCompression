d68f3be3f91a59f0f9e8bb51d4e768882c06b26a
fateme <f.sedighi.24@gmail.com>
2020-05-31 17:23:49 +0430

    Refactoring

---
+++ ORCT3.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+A = np.array([[1 / 2, 1 / 2], [-1, 1]])
+
+
+def compute_orct3(bayer):
+    bayer_number_of_rows = bayer.shape[0]
+    bayer_number_of_columns = bayer.shape[1]
+    final_block = bayer
+    row_index = 0
+    column_index = 0
+    while row_index <= bayer_number_of_rows:
+        while column_index <= (bayer_number_of_columns - 2):
+            y1 = bayer[row_index][column_index]
+            y2 = bayer[row_index][column_index + 2]
+            dy = A[1][0] * y2 + A[1][1] * y1
+            wy = A[0][0] * y2 + A[0][1] * y1
+            final_block[row_index][column_index] = dy
+            final_block[row_index][column_index + 2] = wy
+            column_index += 2
+        row_index += 1
+    return final_block
