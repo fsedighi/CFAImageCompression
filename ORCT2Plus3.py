6d5efea3ef59d21ffa513f1e21f3eb700a947fc2
fateme <f.sedighi.24@gmail.com>
2020-06-09 22:53:52 +0430

    Refactoring

--- ORCT2Plus3.py
+++ ORCT2Plus3.py
@@ -8,17 +8,7 @@ def compute_orct2plus3(bayer):
     final_block = bayer
     row_index = 0
     column_index = 0
-    # while column_index < bayer_number_of_columns:
-    #     while row_index < bayer_number_of_rows - 1:
-    #         w1 = bayer[row_index][column_index]
-    #         w2 = bayer[row_index + 1][column_index]
-    #         d = w1 - w2
-    #         w = w2 + np.floor(d/2)
-    #         final_block[row_index][column_index] = d
-    #         final_block[row_index + 1][column_index] = w
-    #         row_index += 1
-    #     row_index = 0
-    #     column_index += 2
+
     for column_index in range(0, bayer_number_of_columns, 2):
         for row_index in range(bayer_number_of_rows - 1):
             w1 = bayer[row_index][column_index]
