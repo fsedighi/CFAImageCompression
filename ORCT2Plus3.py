a8e0e01694ac2ac9efcbedff13030322dbf5013b
Fatemeh <f.sedighi.24@gmail.com>
2020-08-27 22:43:05 +0430

    ORCT2Plus3 edits

--- ORCT2Plus3.py
+++ ORCT2Plus3.py
@@ -16,15 +16,10 @@ def compute_orct2plus3(bayer, Alocal=None, precisionFloatingPoint=0):
 
     for column_index in range(0, bayer_number_of_columns, 2):
         for row_index in range(bayer_number_of_rows - 1):
-            index = bayer_number_of_rows % 2
             w1 = final_block[row_index][column_index]
             w2 = final_block[row_index + 1][column_index]
             converted_w1_w2 = A @ np.array([w2, w1]).transpose()
-            if index == 0:
-                final_block[row_index][column_index] = np.floor(converted_w1_w2[1])
-                final_block[row_index + 1][column_index] = np.floor(converted_w1_w2[0])
-            elif index == 1:
-                final_block[row_index][column_index] = np.ceil(converted_w1_w2[1])
-                final_block[row_index + 1][column_index] = np.ceil(converted_w1_w2[0])
+            final_block[row_index][column_index] = np.floor(converted_w1_w2[1])
+            final_block[row_index + 1][column_index] = np.floor(converted_w1_w2[0])
 
     return final_block
